4a9b197935a85cdd00462218c38045330e2ca487
Francesco <francesco.lipani@emea.nttdata.com>
2022-04-14 15:00:38 +0200

    primo commit

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 
+import uvicorn as uvicorn
+
 app = FastAPI()
 
 
@@ -11,3 +13,7 @@ async def read_user_me():
 @app.get("/users/{user_id}")
 async def read_user(user_id: str):
     return {"user_id": user_id}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
