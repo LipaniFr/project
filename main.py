7006a9762650c86fee1d4ff8a5f105cf0bb87cad
Francesco <francesco.lipani@emea.nttdata.com>
2022-04-14 17:40:01 +0200

    breakpoint 14/04

--- main.py
+++ main.py
@@ -1,19 +1,27 @@
 from fastapi import FastAPI
-
-import uvicorn as uvicorn
+import uvicorn
+from enum import Enum
+from typing import Optional
+from pydantic import BaseModel
+from typing import Optional
 
 app = FastAPI()
 
-
-@app.get("/users/me")
-async def read_user_me():
-    return {"user_id": "the current user"}
-
-
-@app.get("/users/{user_id}")
-async def read_user(user_id: str):
-    return {"user_id": user_id}
+class Item(BaseModel):
+    name: str
+    description: Optional[str] = None
+    price: float
+    tax: Optional[float] = None
 
 
+class User(BaseModel):
+    username: str
+    full_name: Optional[str] = None
+
+
+@app.put("/items/{item_id}")
+async def update_item(item_id: int, item: Item, user: User):
+    results = {"item_id": item_id, "item": item, "user": user}
+    return results
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
